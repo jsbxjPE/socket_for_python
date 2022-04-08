970cf3c490c764c67091e160d2369b89fa9b6d78
zzs <63991031+jsbxjPE@users.noreply.github.com>
2022-04-08 23:32:18 +0800

    Delete cs directory

--- cs/cs1.py
+++
@@ -1,5 +0,0 @@
-import threading
-import cs2
-cs2.w()
-while 1:
-    print('t1')
--- cs/cs2.py
+++
@@ -1,8 +0,0 @@
-import threading
-def w():
-    while 1:
-        t1 = threading.Thread(target=q())
-        t1.start()
-
-def q():
-    print('t2')
