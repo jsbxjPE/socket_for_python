67f57532d8f5a41a21105e087d2b6d960caaf980
zzs <63991031+jsbxjPE@users.noreply.github.com>
2022-04-08 23:32:28 +0800

    Delete first directory

--- first/run_cliaten.py
+++
@@ -1,4 +0,0 @@
-import c
-host,port='192.168.31.65',3030
-c.clisten.clisten_setup(host,port)
-c.clisten.clisten_start()
--- first/server_setup.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-def setup(host,port,listen_number):
-    s = socket.socket()
-    s.bind((str(host), int(port))) # 绑定ip和端口
-    s.listen(listen_number) # 等待客户端连接
-    print(s)
-    return s
